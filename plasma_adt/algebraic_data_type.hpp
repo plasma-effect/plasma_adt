7a7a9d413324b48d16d682a272f17d5cef9cf3aa
plasma_effect <dtyazsk_windows7@yahoo.co.jp>
2015-12-26 17:03:10 +0900

    ライセンス追加

--- plasma_adt/algebraic_data_type.hpp
+++ plasma_adt/algebraic_data_type.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 
+// Copyright plasma-effect 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+
 #include<boost/variant.hpp>
 #include<boost/optional.hpp>
 
